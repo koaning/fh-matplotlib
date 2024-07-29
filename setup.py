2f7980176f1dc555ba1413ddb59ec1f1bc426b1e
vincent d warmerdam <vincentwarmerdam@gmail.com>
2024-07-29 09:16:43 +0000

    version-bump

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+long_description = (Path(__file__).parent / "README.md").read_text()
 
 setup(
     name="fh-matplotlib",
-    version="0.0.1",
+    description="Make is easy to use matplotlib in FastHTML",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Vincent D. Warmerdam",
+    version="0.0.2",
     packages=find_packages(),
     install_requires=["matplotlib", "python-fasthtml"],
     extras_require={
